0075e9b7ec373d2e12819e1728af145c91a372ff
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2018-12-30 08:37:41 -0500

    .

--- cosbaksol/cosbak/Program.cs
+++ cosbaksol/cosbak/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace cosbak
 {
@@ -7,6 +9,39 @@ namespace cosbak
         static void Main(string[] args)
         {
             Console.WriteLine($"cosbak - Cosmos DB Backup Solution - Version {AppVersion.FullVersion}");
+
+            if (args.Length == 0)
+            {
+                DisplayBasicHelp();
+            }
+            else
+            {
+                BranchCommand(args[0], args.Skip(1));
+            }
+        }
+
+        private static void BranchCommand(string command, IEnumerable<string> args)
+        {
+            switch (command)
+            {
+                case "backup":
+                    Console.WriteLine("Backup...  work in progress");
+
+                    return;
+
+                default:
+                    Console.WriteLine($"Command '{command}' unknown");
+                    DisplayBasicHelp();
+
+                    return;
+            }
+        }
+
+        private static void DisplayBasicHelp()
+        {
+            Console.WriteLine("Here are the base commands:");
+            Console.WriteLine();
+            Console.WriteLine("backup:\t\t\t\tTake a backup of a collection (or database or account)");
         }
     }
 }
