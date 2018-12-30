633d5c06e0c278c69d636c9fd7c04ad5453d09a6
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2018-12-30 08:30:40 -0500

    .

---
+++ cosbaksol/cosbak/AppVersion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cosbak
+{
+    internal class AppVersion
+    {
+        public static string FullVersion { get { return "FULL_VERSION"; } }
+    }
+}
---
+++ cosbaksol/cosbak/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cosbak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"cosbak - Cosmos DB Backup Solution - Version {AppVersion.FullVersion}");
+        }
+    }
+}
