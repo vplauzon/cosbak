04b5ba35d2ce46e4b656b3779ec0057a77a6075f
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2019-05-24 16:28:06 -0400

    Add unit tests to BackupCommandTest

--- cosbaksol/cosbak.unit/Command/BackupCommandTest.cs
+++ cosbaksol/cosbak.unit/Command/BackupCommandTest.cs
@@ -19,5 +19,48 @@ namespace cosbak.unit.Command
             Assert.NotNull(config.CosmosAccount);
             Assert.NotNull(config.StorageAccount);
         }
+
+        [Fact]
+        public async Task CosmosNameAsync()
+        {
+            const string NAME = "MyCosmos";
+            var command = new BackupCommand();
+            var config = await command.ExtractDescriptionAsync(new[] { "-cn", NAME });
+
+            Assert.NotNull(config);
+            Assert.NotNull(config.CosmosAccount);
+            Assert.NotNull(config.StorageAccount);
+            Assert.Equal(NAME, config.CosmosAccount.Name);
+        }
+
+        [Fact]
+        public async Task CosmosNameAndKeyAsync()
+        {
+            const string NAME = "MyCosmos";
+            const string KEY = "123456";
+            var command = new BackupCommand();
+            var config = await command.ExtractDescriptionAsync(new[] { "-cn", NAME, "-ck", KEY });
+
+            Assert.NotNull(config);
+            Assert.NotNull(config.CosmosAccount);
+            Assert.NotNull(config.StorageAccount);
+            Assert.Equal(NAME, config.CosmosAccount.Name);
+            Assert.Equal(KEY, config.CosmosAccount.Key);
+        }
+
+        [Fact]
+        public async Task CosmosKeyAndStorageContainerAsync()
+        {
+            const string KEY = "123456";
+            const string CONTAINER = "acontainer";
+            var command = new BackupCommand();
+            var config = await command.ExtractDescriptionAsync(new[] { "-ck", KEY, "-sc", CONTAINER });
+
+            Assert.NotNull(config);
+            Assert.NotNull(config.CosmosAccount);
+            Assert.NotNull(config.StorageAccount);
+            Assert.Equal(KEY, config.CosmosAccount.Key);
+            Assert.Equal(CONTAINER, config.StorageAccount.Container);
+        }
     }
 }
