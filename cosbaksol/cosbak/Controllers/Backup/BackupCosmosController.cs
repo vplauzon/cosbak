017de033a14e2f25ec1db6f3b5a71e791dce74ed
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2019-05-27 16:52:54 -0400

    Fix...

---
+++ cosbaksol/cosbak/Controllers/Backup/BackupCosmosController.cs
@@ -0,0 +1,19 @@
+﻿using Cosbak.Logging;
+using Cosbak.Cosmos;
+using System;
+using System.Collections.Generic;
+
+namespace Cosbak.Controllers.Backup
+{
+    public class BackupCosmosController : IBackupCosmosController
+    {
+        private readonly IDatabaseAccountFacade _accountFacade;
+        private readonly ILogger _logger;
+
+        public BackupCosmosController(IDatabaseAccountFacade accountFacade, ILogger logger)
+        {
+            _accountFacade = accountFacade ?? throw new ArgumentNullException(nameof(accountFacade)); ;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger)); ;
+        }
+    }
+}
