9cd7644458172e7f3e90755e838458492197717b
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2019-12-14 19:54:08 -0500

    Add LogFile

---
+++ cosbaksol/cosbak/Controllers/LogBackup/LogCollectionBackupController.cs
@@ -0,0 +1,50 @@
+﻿using Cosbak.Cosmos;
+using Cosbak.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosbak.Controllers.Log
+{
+    internal class LogCollectionBackupController
+    {
+        private const int MAX_BATCH_SIZE = 10;
+
+        private readonly ICollectionFacade _collectionFacade;
+        private readonly LogFile _logFile;
+        private readonly ILogger _logger;
+
+        public LogCollectionBackupController(
+            ICollectionFacade collectionFacade,
+            IStorageFacade storageFacade,
+            ILogger logger)
+        {
+            _collectionFacade = collectionFacade;
+            _logFile = new LogFile(
+                storageFacade,
+                collectionFacade.Parent.Parent.AccountName,
+                collectionFacade.Parent.DatabaseName,
+                collectionFacade.CollectionName,
+                logger);
+            _logger = logger;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _logFile.InitializeAsync();
+        }
+
+        public async Task BackupBatchAsync()
+        {
+            var lastUpdateTime = await _collectionFacade.GetLastUpdateTimeAsync(0, MAX_BATCH_SIZE);
+
+            throw new NotImplementedException();
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _logFile.DisposeAsync();
+        }
+    }
+}
---
+++ cosbaksol/cosbak/Controllers/LogBackup/LogFile.cs
@@ -0,0 +1,66 @@
+﻿using Cosbak.Storage;
+using System;
+using System.Threading.Tasks;
+
+namespace Cosbak.Controllers.Log
+{
+    internal class LogFile
+    {
+        #region Inner Types
+        private class Initialized
+        {
+            public Initialized(BlobLease lease)
+            {
+                Lease = lease;
+            }
+
+            public BlobLease Lease { get; }
+        }
+        #endregion
+
+        private readonly IStorageFacade _storageFacade;
+        private readonly string _blobName;
+        private readonly ILogger _logger;
+        private Initialized? _initialized;
+
+        public LogFile(
+            IStorageFacade storageFacade,
+            string accountName,
+            string databaseName,
+            string collectionName,
+            ILogger logger)
+        {
+            _storageFacade = storageFacade.ChangeFolder($"backups/{accountName}/{databaseName}");
+            _blobName = $"{collectionName}.{Constants.LOG_EXTENSION}";
+            _logger = logger;
+        }
+
+        public async Task InitializeAsync()
+        {
+            if (_initialized != null )
+            {
+                throw new InvalidOperationException("InitializeAsync has already been called");
+            }
+
+            var lease = await _storageFacade.GetLeaseAsync(_blobName);
+
+            if (lease == null)
+            {
+                throw new CosbakException($"Can't lease '{_blobName}' blob");
+            }
+            else
+            {
+                _initialized = new Initialized(lease);
+            }
+        }
+
+        public async Task DisposeAsync()
+        {
+            if (_initialized == null)
+            {
+                throw new InvalidOperationException("InitializeAsync hasn't been called");
+            }
+            await _initialized.Lease.ReleaseLeaseAsync();
+        }
+    }
+}
